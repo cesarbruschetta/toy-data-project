2dc775e30e114191c85f90aa0605c04b64a1a37e
Cesar Augusto <cesar.bruschetta@olxbr.com>
2025-03-10 17:59:08 -0300

    Add Deployment api andy

---
+++ dev/kafka/init-kafka.sh
@@ -0,0 +1,12 @@
+# blocks until kafka is reachable
+kafka-topics --bootstrap-server kafka:29092 --list
+
+echo -e 'Creating kafka topics'
+kafka-topics --bootstrap-server kafka:29092 \
+    --create --if-not-exists \
+    --replication-factor 1 \
+    --partitions 1 \
+    --topic toydata-topic-temperature-v1
+
+echo -e 'Successfully created the following topics:'
+kafka-topics --bootstrap-server kafka:29092 --list

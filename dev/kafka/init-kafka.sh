baeace9c1f84c1363ef8dfbd9c3d212034fd9279
Cesar Augusto <cesar.bruschetta@olxbr.com>
2025-04-23 19:18:46 -0300

    update docker-compose.yml

--- dev/kafka/init-kafka.sh
+++ dev/kafka/init-kafka.sh
@@ -5,7 +5,7 @@ echo -e 'Creating kafka topics'
 kafka-topics --bootstrap-server kafka:29092 \
     --create --if-not-exists \
     --replication-factor 1 \
-    --partitions 1 \
+    --partitions 2 \
     --topic toydata-topic-temperature-v1
 
 echo -e 'Successfully created the following topics:'

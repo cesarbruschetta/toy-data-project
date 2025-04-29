830ba025ecda21a1ff34b6d1506d9b2b3dc0e06c
Cesar Augusto <cesar.bruschetta@olxbr.com>
2025-04-29 17:51:34 -0300

    add hive metastore and trino

---
+++ dev/hive/init-metastore.sql
@@ -0,0 +1,24 @@
+-- Create the database and table for the raw data
+CREATE DATABASE toy_data_raw
+LOCATION 's3a://dl-test-localstack/raw/'
+
+-- Create the table for the raw data
+CREATE EXTERNAL TABLE IF NOT EXISTS toy_data_raw.sensor_readings (
+    sensor_id STRING,
+    temperature DOUBLE,
+    humidity DOUBLE,
+    heat_index DOUBLE,
+    pressure DOUBLE,
+    altitude DOUBLE,
+    temperature_bmp DOUBLE,
+    `timestamp` BIGINT
+)
+PARTITIONED BY (dt STRING)
+STORED AS PARQUET
+LOCATION 's3a://dl-test-localstack/raw/toydata-topic-temperature-v1'
+TBLPROPERTIES (
+    'parquet.compression' = 'ZSTD'
+);
+
+-- Load the data into the table
+MSCK REPAIR TABLE toy_data_raw.sensor_readings;

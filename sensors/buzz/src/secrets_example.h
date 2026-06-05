9ff90601849d94d38a58f8068272795e9329d52f
Cesar Augusto <cesarabruschetta@gmail.com>
2026-06-05 12:31:20 -0300

    fix(secrets): atualizar URL do domínio personalizado para o API Gateway AWS

--- sensors/buzz/src/secrets_example.h
+++ sensors/buzz/src/secrets_example.h
@@ -10,7 +10,7 @@ const char* SENSOR_ID = "buzz";
 
 // Endpoint do API Gateway AWS
 // Obtenha após o terraform apply: make infra-api-url
-// Se tiver custom domain configurado: https://andy-api.k8s.your-domain.com/temperature
+// Se tiver custom domain configurado: https://andy-api.aws.our-cluster.ovh/temperature
 const char* ANDY_API = "https://<id>.execute-api.us-east-1.amazonaws.com/v1/temperature";
 
 #endif
